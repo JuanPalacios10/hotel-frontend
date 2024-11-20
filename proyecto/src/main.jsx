2cdcdefe07970be411b93c51008fd18a195d71c5
MaicolRodriguez12 <michael.arana@correounivalle.edu.co>
2024-11-19 23:55:33 -0500

    menu secciones

--- proyecto/src/main.jsx
+++ proyecto/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
+import { Secciones } from './components/menu secciones/secciones'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>Hola Mundo</h1>
+    <Secciones />
   </StrictMode>
 )
+
