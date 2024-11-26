7ad61d040a577574375a5617100af2bb3e02acf6
MaicolRodriguez12 <michael.arana@correounivalle.edu.co>
2024-11-25 22:57:06 -0500

    Registro

--- proyecto/src/App.jsx
+++ proyecto/src/App.jsx
@@ -1,12 +1,17 @@
 import { AppRouter } from './AppRouter'
 import { Header } from './components/Header'
 import { BrowserRouter } from 'react-router-dom'
+import { Modal } from './components/Modal'
+import { Registro } from './pages/Registro/Registro'
 
 export function App() {
   return (
     <BrowserRouter>
       <Header />
       <AppRouter />
+      <Modal>
+        <Registro> </Registro>
+      </Modal> 
     </BrowserRouter>
   )
 }
--- proyecto/src/context/ModalContext.jsx
+++ proyecto/src/context/ModalContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from 'react'
 export const modalContext = createContext()
 
 export function ModalContextProvider({ children }) {
-  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(true)
 
   const openModal = () => setModal(true)
 
