9ac7902ae57023eb6a223a5c6cd032783e4c96a5
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2024-12-11 01:49:14 -0500

    Implementación de la conexion de inicio de sesion y registro

--- proyecto/src/utilities/formatData.js
+++ proyecto/src/utilities/formatData.js
@@ -1,10 +1,23 @@
 export const formatLoginData = (data) => {
   if (!(data instanceof Object)) return
 
-  const dataArr = Object.entries(data)[1]
+  const dataKeys = Object.keys(data)
 
   return {
-    username: dataArr[0],
-    password: dataArr[1]
+    username: data[dataKeys[0]],
+    password: data[dataKeys[1]]
+  }
+}
+
+export const formatRegisterData = (data) => {
+  if (!(data instanceof Object)) return
+
+  const dataKeys = Object.keys(data)
+
+  return {
+    username: data[dataKeys[0]],
+    email: data[dataKeys[1]],
+    phone: data[dataKeys[2]],
+    password: data[dataKeys[3]]
   }
 }
